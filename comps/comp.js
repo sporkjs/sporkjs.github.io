c373a7139db23f5c7d585e0b25dc7c3a905909ed
sporkjs <sporkjs@users.noreply.github.com>
2016-05-13 07:22:14 -0500

    Add files via upload

--- comps/comp.js
+++ comps/comp.js
@@ -56,12 +56,42 @@ function doit(){
 	thePreview.innerHTML=r;
 	MathJax.Hub.Typeset();
 }
+
+function showall(){
+	var i,j,k,l;
+	var used=[];
+	var adding=[];
+	var problemsToAdd=[];
+	var r='';
+	for (i=0;i<comps.length;i++)
+	if (document.getElementById('check'+i).checked){
+		r+='\r'+names[i]+'\r';
+		for (j=0;j<comps[i].length;j++){
+			r+='\r Type '+j+' ('+nums[i][j]+')'+'\r \\begin{enumerate}';
+			for (k=0;k<comps[i][j].length;k++)
+				r+=comps[i][j][k]+'\r';
+			r+='\\end{enumerate}\r'
+		}
+	}
+	theLatex.value=r;
+//	theLatex.style.height = (theLatex.scrollHeight)+"px";
+	r=r.replace(/\\item/g, '<li>');
+	r=r.replace(/\\begin{enumerate}/g, '<ol>')
+	r=r.replace(/\\end{enumerate}/g, '</ol>')
+	r=r.replace(/\\begin{quotation}/g, '<center>')
+	r=r.replace(/\\end{quotation}/g, '</center>')
+	r=r.replace(/\\noindent/g, '')
+	//r=r.replace(/\\\\/g, '<br>')
+
+	thePreview.innerHTML=r;
+	MathJax.Hub.Typeset();
+}
 function setup(){
 	var i;
 	var r='';
 	for (i=0;i<comps.length;i++)
 		r+='<label for="check'+i+'"> <input type="checkbox" id="check'+i+'"> '+names[i].replace('Competency ', '')+' </label><br>';
-	r+="<br><button onclick='doit();'>Create Test</button>";
+	r+="<br><button onclick='doit();'>Create Test</button><button onclick='showall();'>All Types</button>";
 	controlPanel.innerHTML=r;
 }
 window.onload=function(){setup();}
