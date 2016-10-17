ae5d08318a9a84605e1d77024bc4279a03e77295
sporkjs <sporkjs@users.noreply.github.com>
2016-10-17 15:39:18 -0500

    Add files via upload

--- comps/comp.js
+++ comps/comp.js
@@ -12,22 +12,29 @@ function doit(){
 	var problemsToAdd=[];
 	var r='';
 	var top='\
-\\documentclass[10pt]{extreport} \r\
-\\usepackage[height=9in,width=7in]{geometry}\r\
-\\usepackage{amsmath}\r\
-\\usepackage{multicol}\r\
-\\usepackage{enumerate}\r\
-\\usepackage{tikz,pgfplots}\r\
-\\usepgfplotslibrary{statistics}\r\
+\\documentclass[12pt]{extreport} \r\
+\\usepackage[height=10in,width=7.5in]{geometry}\r\
 \\everymath={\\displaystyle} \r\
 \\parindent = 0 in \r\
 \\pagestyle{empty}\r\
+\\usepackage{amsmath}\r\
+\\usepackage{graphicx}\r\
+\\usepackage{multicol}\r\
+\\usepackage{color}\r\
+\\usepackage{tikz,pgfplots}\r\
+\\usepgfplotslibrary{statistics}\r\
+\\def\\blank{\\underline{ \\hskip 1 in} }\r\
+\\def\\test{TESTNUMBER}\r\
+\\def\\version{TESTVERSION}\r\
+\\def\\thetest{{\\bf Math XXX Test \\test \\version} \\hfill name: \\blank\\blank}\r\
+\\def\\mynewpage{\\eject \\thetest\\\\}\r\
 \\begin{document}\r\
-	';
+\\thetest\\\\\r\r';
+
 	var bottom='\r\\end{document}';
 	for (i=1;i<comps.length;i++)
 	if (document.getElementById('check'+i).checked){
-		r+='\r'+names[i]+'\r';
+		r+='\r\r{\\bf '+names[i]+' \\blank}\r';
 		r+=r='\\begin{enumerate}\r';
 		problemsToAdd=[];
 		for (j=0;j<comps[i].length;j++){
@@ -55,7 +62,7 @@ function doit(){
 			used[k]=1;
 			j++;
 		}
-		r+='\\end{enumerate}\r\\eject\r';
+		r+='\\end{enumerate}\r\\mynewpage\r\r';
 	}
 	theLatex.value=top+r+bottom;
 //	theLatex.style.height = (theLatex.scrollHeight)+"px";
@@ -79,18 +86,24 @@ function showall(){
 	var problemsToAdd=[];
 	var r='';
 	var top='\
-\\documentclass[10pt]{extreport} \r\
-\\usepackage[height=9in,width=7in]{geometry}\r\
-\\usepackage{amsmath}\r\
-\\usepackage{multicol}\r\
-\\usepackage{enumerate}\r\
-\\usepackage{tikz,pgfplots}\r\
-\\usepgfplotslibrary{statistics}\r\
+\\documentclass[12pt]{extreport} \r\
+\\usepackage[height=10in,width=7.5in]{geometry}\r\
 \\everymath={\\displaystyle} \r\
 \\parindent = 0 in \r\
 \\pagestyle{empty}\r\
+\\usepackage{amsmath}\r\
+\\usepackage{graphicx}\r\
+\\usepackage{multicol}\r\
+\\usepackage{color}\r\
+\\usepackage{tikz,pgfplots}\r\
+\\usepgfplotslibrary{statistics}\r\
+\\def\\blank{\\underline{ \\hskip 1 in} }\r\
+\\def\\test{TESTNUMBER}\r\
+\\def\\version{TESTVERSION}\r\
+\\def\\thetest{{\\bf Math XXX Test \\test \\version} \\hfill name: \\blank\\blank}\r\
+\\def\\mynewpage{\\eject \\thetest\\\\}\r\
 \\begin{document}\r\
-	';
+\\\\\\\r\r';
 	var bottom='\r\\end{document}';
 	for (i=1;i<comps.length;i++)
 	if (document.getElementById('check'+i).checked){
