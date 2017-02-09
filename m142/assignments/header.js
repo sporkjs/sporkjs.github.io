f5981e5d972cea4809ce1b428bd22ac3e0d5e74e
sporkjs <sporkjs@users.noreply.github.com>
2017-02-09 11:45:16 -0600

    Add files via upload

--- m142/assignments/header.js
+++ m142/assignments/header.js
@@ -5,7 +5,7 @@ function showHeader(){
 	<div id="topNavDivContent">\
 	  <a href="http://jwsnow.com"><span class="navButton" id="jwsButton">jwsnow.com</span></a>\
 	| <a href="../index.html"><span class="navButton" id="homeButton">home</span></a>\
-	| <a href="index.html"><span class="navButton" id="editorButton">spork editor</span></a>\
+	| <a href="../sporkeditor/index.html"><span class="navButton" id="editorButton">spork editor</span></a>\
 	| <a href="../studentlinks.html"><span class="navButton" id="linksButton">students</span></a> \
 </div></div>\
 <div id="mainContentDiv">	\
