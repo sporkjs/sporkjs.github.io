7e6874b4cbb432888aeb9e2de28b5de06b446d0c
sporkjs <sporkjs@users.noreply.github.com>
2017-04-24 21:13:06 -0500

    Add files via upload

---
+++ 142/jensen/header.js
@@ -0,0 +1,18 @@
+function showHeader() {
+
+  document.write("<div class='headerDiv'>\
+      <p>Math 142 Project</p>\
+     </div>\
+     <div class='bodyDiv'>");
+
+}
+
+
+function showFooter() {
+
+  document.write("</div>\
+     <div class='footerDiv'>\
+      <p>To get to the index click <a href=index.html>here</a>.<br><a href='../index.html'>142 Page</a></p>\
+     </div>");
+
+}
