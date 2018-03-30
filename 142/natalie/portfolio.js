223e81f54639380316e2c37088bb8394b645ff56
sporkjs <sporkjs@users.noreply.github.com>
2018-03-30 10:27:13 -0500

    Update 142

--- 142/natalie/portfolio.js
+++
@@ -1,6 +0,0 @@
-function writeHeader(){
-	document.write("<div class='title'><h1>Natalie Lehenbauer's Portfolio Page</h1><h2>Spring 2015</h2></div><div class='content'>");
-}
-function writeFooter(){
-	document.write("</div><div class='footer'><a href='../index.html'>LOOK AT ALL MY WORK HERE!</a><br>Almost Thomas Approved!</div>")
-}
